1f30532505a7710a3c349f6a8ca0ef8704a3c5da
borisding <boris.ding82@outlook.com>
2022-11-09 18:01:56 +0800

    add lint tooling

---
+++ .stylelintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-css-modules',
+    'stylelint-prettier/recommended'
+  ],
+  rules: {
+    'selector-class-pattern': null,
+    'color-function-notation': null
+  }
+};
